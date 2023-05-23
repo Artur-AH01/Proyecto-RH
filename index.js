ad42a8c4544200a54790bbc7fad0e69c4205de44
Artur-AH01 <artur.ah0108@gmail.com>
2023-05-23 13:39:03 -0600

    Acceso de datos

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 //Dependencias
+
 const express = require('express');
 const app = express();
 
 
 app.get("/", (req, res, next)=>{
     res.status(200);
-    res.send("Bienvenido al servidor");
+    res.send("Bienvenido");
 });
 
-app.listen(3000, ()=>{
+app.get("/:name", (req, res, next)=>{
+    nam = req.params.name;
+    res.status(200);
+    res.send("Hola, " + nam);
+});
+
+
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Server is running');
 
 });
