4110c6068601e9bf707bc6c7d36773159c66f9e9
Artur-AH01 <artur.ah0108@gmail.com>
2023-05-23 12:56:32 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Dependencias
+const express = require('express');
+const app = express();
+
+
+app.get("/", (req, res, next)=>{
+    res.status(200);
+    res.send("Bienvenido al servidor");
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is running');
+
+});
